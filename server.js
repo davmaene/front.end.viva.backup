749302b980d00748d6ecf6cc7569426e85bb4b4b
david_maene <davidmened@gmail.com>
2020-04-08 00:01:09 +0200

    still testing my front-end

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+app.use(express.static('./dist/vivaRDC'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/vivaRDC/'}
+  );
 });
- 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
