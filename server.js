3d127c9a9ed5464ce45c2727b904c4685d9ad3ec
david_maene <davidmened@gmail.com>
2020-04-08 11:21:13 +0200

    make again again

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 app.use(express.static('./dist/vivaRDC'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/vivaRDC/'}
   );
 });
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
